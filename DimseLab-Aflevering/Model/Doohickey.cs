5cea9b0016c9fc3168f694164eaa91f7cc7a3598
lassegh <lasse.groenbech@gmail.com>
2018-11-26 12:58:26 +0100

    Initial commit

---
+++ DimseLab-Aflevering/Model/Doohickey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimseLab_Aflevering.Model
+{
+    class Doohickey
+    {
+        // Dimmerduten
+    }
+}
---
+++ DimseLab-Aflevering/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimseLab_Aflevering
+{
+    class User
+    {
+        // Ich bin der user class
+    }
+}

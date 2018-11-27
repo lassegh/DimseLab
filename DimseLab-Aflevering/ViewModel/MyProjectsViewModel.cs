b737f6f87c10448cd3323903efc3555c8f87e56b
lassegh <lasse.groenbech@gmail.com>
2018-11-27 17:29:40 +0100

    lG

---
+++ DimseLab-Aflevering/ViewModel/MyProjectsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DimseLab_Aflevering.Annotations;
+using DimseLab_Aflevering.Model;
+
+namespace DimseLab_Aflevering.ViewModel
+{
+    class MyProjectsViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Project> _myProjects = new ObservableCollection<Project>();
+        private Project _selectedProject;
+
+        public ObservableCollection<Project> MyProjects { get; set; }
+        public Project SelectedProject
+        {
+            get { return _selectedProject; }
+            set { _selectedProject = value; }
+        }
+
+        public MyProjectsViewModel()
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

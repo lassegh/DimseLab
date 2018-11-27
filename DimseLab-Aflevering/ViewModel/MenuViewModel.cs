cb47ea5124001b3f2a086b1f1324a4ed72a5d383
lassegh <lasse.groenbech@gmail.com>
2018-11-27 12:06:59 +0100

    fucked XAML

---
+++ DimseLab-Aflevering/ViewModel/MenuViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+
+namespace DimseLab_Aflevering.ViewModel
+{
+    class MenuViewModel
+    {
+        public RelayCommand BrowseButton { get; set; }
+        public RelayCommand ManageProjectsButton { get; set; }
+        public RelayCommand UserProfileButton { get; set; }
+
+        public MenuViewModel()
+        {
+            
+        }
+    }
+}

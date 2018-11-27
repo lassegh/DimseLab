f4e778d1bc004af2bfa5c725d67f967bdfd87414
Kjergaard <KjergaardVFX@gmail.com>
2018-11-27 10:36:00 +0100

    Update Helper.cs

--- DimseLab-Aflevering/Model/Helper.cs
+++ DimseLab-Aflevering/Model/Helper.cs
@@ -9,6 +9,68 @@ namespace DimseLab_Aflevering.Model
     class Helper
     {
 
-        // Og medløberen
+        List<Project> _projectList = new List<Project>();
+
+        List<Doohickey> _doohickeyList = new List<Doohickey>();
+
+        List<User> _userList = new List<User>();
+
+
+        public Helper()
+        {
+          
+            
+
+
+        }
+
+
+        public void WriteProjectData()
+        {
+
+        }
+
+        public void ReadProjectData()
+        {
+
+        }
+
+        public void WriteDoohickeyData()
+        {
+
+        }
+
+        public void ReadDoohickeyData()
+        {
+
+        }
+
+        public void WriteUserData()
+        {
+
+        }
+
+        public void ReadUserData()
+        {
+
+        }
+
+        public List<User> UserList
+        {
+            get { return _userList; }
+            set { _userList = value; }
+        }
+
+        public List<Doohickey> DoohickeyList
+        {
+            get { return _doohickeyList; }
+            set { _doohickeyList = value; }
+        }
+
+        public List<Project> ProjectList
+        {
+            get { return _projectList; }
+            set { _projectList = value; }
+        }
     }
 }

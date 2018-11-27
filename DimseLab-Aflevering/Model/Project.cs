3444999d053cff5c5fa7cf5bcdf4eb5a64817ae2
Lars du Jour <lars66c1@edu.easj.dk>
2018-11-27 10:48:11 +0100

    Kode til at  afslutte projekter

--- DimseLab-Aflevering/Model/Project.cs
+++ DimseLab-Aflevering/Model/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace DimseLab_Aflevering.Model
         private DateTime _projectBeginDate;
         private DateTime _projectEndDate;
 
+        private List<User> _projectMembers;
         private List<Doohickey> _borrowedItems;
 
         private bool isFinished;
@@ -58,7 +60,15 @@ namespace DimseLab_Aflevering.Model
             set { isFinished = value; }
         }
 
+        public List<User> ProjectMembers
+        {
+            get { return _projectMembers; }
+            set { _projectMembers = value; }
+        }
+
         #endregion
+
+
 
         public Project(string inputName, string inputDescription, DateTime inputEndDate)
         {
@@ -69,5 +79,32 @@ namespace DimseLab_Aflevering.Model
             IsFinished = false; //for den er jo ikke færdig når den lige er blevet skabt
         }
 
+
+
+        #region Methods
+
+        //før vi kan lukke et projekt, skal vi tjekke om det har nogle lån
+        public void CheckProjectForLoans()
+        {
+            if(BorrowedItems.Count == 0) //hvis der er 0 nul items i listen af lån
+            {
+                FinishProject(); //så kalder vi koden der afslutter projektet
+            }
+            else
+            {
+                Debug.Write("ERROR CODE LARS: Projektet har stadig udlån");
+            }
+        }
+
+
+        //at slutte et projekt
+        public void FinishProject()
+        {
+            ProjectMembers.Clear(); //fjerner alle elementer fra listen
+            IsFinished = true;
+
+        }
+
+        #endregion
     }
 }

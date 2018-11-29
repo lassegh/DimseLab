e006cdc97a3f4a55badb0ef34cbe0c7bf4c83ea9
Lars du Jour <lars66c1@edu.easj.dk>
2018-11-29 13:23:12 +0100

    WriteProjectData
    Jeg undskylder på forhånd

--- DimseLab-Aflevering/Model/Helper.cs
+++ DimseLab-Aflevering/Model/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,10 +27,49 @@ namespace DimseLab_Aflevering.Model
         }
 
 
+        //her var lars
+        //alt andet lige, så undskylder han for den følgende WriteProjectData
         public void WriteProjectData()
         {
 
+            //først laver vi en midlertidig liste til at holde vores projekt data. Det gør vi for at undgå at der sker et uheld og at alt dataen forsvinder
+            List<Project> TempProjectData = new List<Project>();
+            TempProjectData = ProjectList;
+
+            //vi gemmer projektdata som strings. Med BuildFile bygger vi en string bestående af andre strings lavet af BuildLine
+            StringBuilder BuildFile = new StringBuilder();
+
+
+            //til sidst sætter vi info for hvilken fil og hvor den er
+            
+
+
+            foreach (Project ITEM in TempProjectData)
+            {
+                //vi gemmer projektdata som strings i et tekstfil
+                StringBuilder BuildLine = new StringBuilder();
+
+                // \t betyder tab
+                BuildLine.Append(ITEM.Name + "\t");
+                BuildLine.Append(ITEM.Description + "\t");
+                BuildLine.Append(ITEM.ProjectMembers.ToString() + "\t");
+                BuildLine.Append(ITEM.ProjectBeginDate + "\t");
+                BuildLine.Append(ITEM.ProjectEndDate + "\t");
+                BuildLine.Append(ITEM.IsFinished.ToString() + "\t");
+                BuildLine.Append(ITEM.BorrowedItems.ToString() + "\n"); // \n betyder ny linie, og er også der hvor siger "her slutter objektet, nu kommer der et nyt" 
+
+                //så tager vi den lange streng i BuildLine og appender den ind i BuildFile
+                BuildFile.Append(BuildLine.ToString());
+            }
+
+            //og så skriver vi
+            System.IO.File.WriteAllText(@"C:\Dimselab\ProjectData.txt", BuildFile.ToString());
+
         }
+
+
+
+
 
         // Her læser vi hele vores "database / Dummydata" af projekter fra, som bliver stoppet i en ny liste
         // som bliver brugt i både "Browse" og i "MyProjects". Men "MyProjects" bliver filtreret og sat i en ny liste

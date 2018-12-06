f70afe171b2ddcdb3c5575d56f9b6133f7aa8544
Kjergaard <KjergaardVFX@gmail.com>
2018-12-06 12:02:49 +0100

    Alternativ måde at gemme data på istedet for XML hvis det ikke virker.

---
+++ DimseLab-Aflevering/Model/JSONReadWrite.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Popups;
+using DimseLab_Aflevering.Model;
+using Newtonsoft.Json;
+
+namespace DimseLab_Aflevering
+{
+    class JsonReadWrite
+    {
+
+        private static string JsonFileName = "Notes.json";
+
+        public static async void SaveNotesAsJsonAsync(ObservableCollection<Project> notes)
+        {
+            string notesJsonString = JsonConvert.SerializeObject(notes);
+            SerializeNotesFileAsync(notesJsonString, JsonFileName);
+        }
+
+        public static async Task<List<Project>> LoadNotesFromJsonAsync()
+        {
+            string notesJsonString = await DeserializeNotesFileAsync(JsonFileName);
+            if (notesJsonString != null)
+                return (List<Project>)JsonConvert.DeserializeObject(notesJsonString, typeof(List<Project>));
+            return null;
+        }
+
+        private static async void SerializeNotesFileAsync(string notesJsonString, string fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, notesJsonString);
+        }
+
+        private static async Task<string> DeserializeNotesFileAsync(string fileName)
+        {
+            try
+            {
+                StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return await FileIO.ReadTextAsync(localFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageDialogHelper.Show("Loading for the first time? - Try Add and Save some Notes before trying to Save for the first time", "File not Found");
+                return null;
+            }
+        }
+
+        private class MessageDialogHelper
+        {
+            public static async void Show(string content, string title)
+            {
+                MessageDialog messageDialog = new MessageDialog(content, title);
+                await messageDialog.ShowAsync();
+            }
+        }
+
+    }
+}

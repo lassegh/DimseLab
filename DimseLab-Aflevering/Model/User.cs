26ee961b9ae8e38b4fcaa980b5e35d78ebc92e5a
lassegh <lasse.groenbech@gmail.com>
2018-12-08 17:19:32 +0100

    Knapper og farver

--- DimseLab-Aflevering/Model/User.cs
+++ DimseLab-Aflevering/Model/User.cs
@@ -35,64 +35,6 @@ namespace DimseLab_Aflevering
             Password = password;
         }
 
-        /*//login system:
-        
-        public sealed partial class MainPage
-        {
-            public MainPage()
-            {
-                this.InitializeComponent();
-            }
-
-            private void Button_Click(object sender, RoutedEventArgs e)
-            {
-                //txtUsername og txtPassword bindes til en TextBox og PasswordBox
-                string username, password;
-                username = txtUsername.Text;
-                password = txtPassword.Password;
-                if (username == "admin@easj.dk" && password == "1234")
-                {
-                    // bruger vises nu som "logged in" og man vises videre til startsiden
-                    //Debug.Write("Success");
-                }
-                else
-                {
-                    // mangler evt. en pop-up message med "error"
-                    //Debug.Write("Error");
-                }
-            }
-        }*/
-
-        /*
-        // A fix for the async method "AddNewUser"
-        #region FixAsyncAddNewUser
-
-        public void CallAddNewUser()
-        {
-            AddNewUser();
-        }
-
-        #endregion
-
-        
-        //Method that adds a new user or otherwise tells the user if they used a wrong email
-        public async Task AddNewUser()
-        {
-            if (UserInputEmail.Contains("@easj.dk".ToLower()) && string.IsNullOrWhiteSpace(UserInputEmail) || string.IsNullOrWhiteSpace(UserInputFirstName) || string.IsNullOrWhiteSpace(UserInputLastName))
-            {
-                // TODO er det ikke kun i dette tilfælde, der skal bruges async?
-                // Wrong email used
-                var dialog = new MessageDialog("Invalid Email Used");
-                await dialog.ShowAsync();
-            }
-            else
-            {   // Adds new user
-                // TODO oprettelse af denne type object skal ikke ske her
-                UserList.Add(new User(UserInputFirstName, UserInputLastName, UserInputNumber, UserInputEmail));
-            }
-        }
-        */
-
         #region Get and Set Properties
 
         public string FirstName

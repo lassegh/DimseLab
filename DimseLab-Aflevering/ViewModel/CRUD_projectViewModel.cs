87d498a02b36ce412a01f02c5e0e6bd563380f31
Lars du Jour <lars66c1@edu.easj.dk>
2018-12-09 16:17:26 +0100

    Admin CRUD påbegyndt

---
+++ DimseLab-Aflevering/ViewModel/CRUD_projectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimseLab_Aflevering.ViewModel
+{
+    class CRUD_projectViewModel
+    {
+    }
+}
